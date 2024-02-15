ec3c98bfdaaee577d5e885a135887e4d5e72addb
BryanOchoa <bochoa314@gmail.com>
2024-02-15 18:38:03 -0500

    created form for contact as well as made javascript to validate them client side.

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function toggleMenu(){
     menu.classList.toggle('open')
     icon.classList.toggle('open')
 }
+
+function validateform (){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if(name.length < 3 || !/^[a-zA-Z ]+$/.test(name)) {
+        alert("Please enter a valid name with more than 3 characters and no numbers or symbols.");
+        return false;
+    }
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+
+    if (message.length > 600) {
+        alert("Message must be 600 characters or less.");
+        return false;
+    }
+
+    return true;
+}
